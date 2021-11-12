3429058e7c391a0f642e337fe81aa956dc3eaf6d
Miguel Mano <miguelmanofernandes@gmail.com>
2021-11-12 16:25:25 +0000

    Disallow script injection on non-YouTube pages

--- chromium/service.js
+++ chromium/service.js
@@ -1,13 +1,20 @@
+const YT_VIDEO_URL = "https://www.youtube.com/watch?v=";
+
 chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.scripting.executeScript({
-    target: { tabId: details.tabId },
-    files: ["script.js"],
-  });
+  if (details.url.includes(YT_VIDEO_URL)) {
+    executeScript(details.tabId);
+  }
 });
 
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  if (details.url.includes(YT_VIDEO_URL)) {
+    executeScript(details.tabId);
+  }
+});
+
+function executeScript(tabId) {
   chrome.scripting.executeScript({
-    target: { tabId: details.tabId },
+    target: { tabId },
     files: ["script.js"],
   });
-});
+}
